de0454eb758bc219f53d8642a0ce3583e15555fb
Loddfafnir12 <vincent.vanderhoeven10@gmail.com>
2025-09-12 15:14:14 +0200

    add modification for the form in html + js for the close modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,14 +10,20 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelectorAll(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalClose.forEach((btn) => btn.addEventListener("click",hideModal));
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
+// hide modal form
+function hideModal(){
+  modalbg.style.display ="none";
+}
 
