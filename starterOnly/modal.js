35638407b511b173003e60a68e77da5bdeb5bd7b
Loddfafnir12 <vincent.vanderhoeven10@gmail.com>
2025-09-16 18:50:17 +0200

    some test on the js + message for the cgu checked or not checked

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,6 +7,8 @@ function editNav() {
   }
 }
 
+// Form data
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -27,3 +29,60 @@ function hideModal(){
   modalbg.style.display ="none";
 }
 
+function validate(event) {
+  if (event) event.preventDefault();
+
+  const firstname = document.getElementById("first").value.trim();
+  const lastname = document.getElementById("last").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const radios = document.querySelectorAll('input[name="location"]');
+  const cgu = document.getElementById("checkbox1");
+  
+  let isValid = true;
+
+  if (firstname.length < 2) {
+    console.log("Le prénom doit contenir au moins 2 caractères.");
+    isValid = false;
+  }
+
+  if (lastname.length < 2) {
+    console.log("Le nom doit contenir au moins 2 caractères.");
+    isValid = false;
+  }
+
+  if (email === "") {
+    console.log("L'email doit être rempli.");
+    isValid = false;
+  }
+  let locationSelected = false;
+  radios.forEach((radio) => {
+    if (radio.checked) locationSelected = true;
+  });
+
+  if (!locationSelected) {
+    console.log("Vous devez sélectionner un tournoi.");
+    isValid = false;
+  }
+  if (!cgu.checked){
+    console.log("cgu not checked")
+    isValid = false;
+  }
+
+  
+
+  if (isValid) {
+    console.log("Formulaire valide !");
+    console.log("Prénom :", firstname);
+    console.log("Nom :", lastname);
+    console.log("Email :", email);
+    console.log("Tournoi choisi :", document.querySelector('input[name="location"]:checked').value);
+    console.log("cgu value : ",cgu)
+
+    
+  } else {
+    console.log("⚠ Formulaire invalide, corrigez les erreurs ci-dessus.");
+  }
+
+  // Ne jamais fermer la modal
+  return false; 
+}
